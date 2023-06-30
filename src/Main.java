1dfd7027e8fb938d631717d03643e33d20ced901
devolden <altynbek.shakirov695@gmail.com>
2023-06-30 09:48:21 +0300

    task 2 completed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import tasks.Tasks;
 import tasks.taskImpl.TaskN1;
+import tasks.taskImpl.TaskN2;
 
 public class Main {
     public static void main(String[] args) {
-        Tasks taskN1 = new TaskN1();
+        TaskN1 taskN1 = new TaskN1();
+        TaskN2 taskN2 = new TaskN2();
+        taskN2.task();
+
 
     }
 }
